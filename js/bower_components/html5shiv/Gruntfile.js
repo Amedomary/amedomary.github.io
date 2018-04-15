95f81b66b6f156570d90273adf3b489cfea6105f
Maxim G <amed@mail.ru>
2018-04-15 22:40:08 +0400

    fix.speed

--- js/bower_components/html5shiv/Gruntfile.js
+++ js/bower_components/html5shiv/Gruntfile.js
@@ -1,1 +1,61 @@
-module.exports=function(s){s.initConfig({pkg:s.file.readJSON("package.json"),bower:s.file.readJSON("bower.json"),copy:{demo:{files:[{expand:!0,src:["src/*"],dest:"dist/",filter:"isFile",flatten:!0}]}},uglify:{options:{beautify:{ascii_only:!0},preserveComments:"some"},html5shiv:{files:[{expand:!0,cwd:"src/",src:["**/*.js"],dest:"dist/",ext:".min.js"}]}},watch:{js:{files:["src/**/*.js"],tasks:["copy","uglify","bytesize"]}},bytesize:{all:{src:["dist/**.min.js"]}}}),s.loadNpmTasks("grunt-contrib-copy"),s.loadNpmTasks("grunt-contrib-uglify"),s.loadNpmTasks("grunt-contrib-watch"),s.loadNpmTasks("grunt-bytesize"),s.registerTask("default",["copy","uglify","bytesize","watch"])};
+/*global module:false*/
+module.exports = function(grunt){
+
+	
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		bower: grunt.file.readJSON('bower.json'),
+		copy: {
+			demo: {
+				files: [
+					{expand: true, src: ['src/*'], dest: 'dist/', filter: 'isFile', flatten: true}
+				]
+			}
+		},
+
+		uglify: {
+			options: {
+				beautify: {
+					ascii_only : true
+				},
+				preserveComments: 'some'
+			},
+			html5shiv: {
+				files: [{
+					expand: true,     // Enable dynamic expansion.
+					cwd: 'src/',      // Src matches are relative to this path.
+					src: ['**/*.js'], // Actual pattern(s) to match.
+					dest: 'dist/',   // Destination path prefix.
+					ext: '.min.js'
+				}]
+			}
+		},
+		watch: {
+			js: {
+				files: ['src/**/*.js'],
+				tasks: ['copy', 'uglify', 'bytesize']
+			}
+		},
+		bytesize: {
+			all: {
+				src: [
+					'dist/**.min.js'
+				]
+			}
+		}
+	});
+
+	
+	// Default task.
+
+	
+
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-bytesize');
+
+	grunt.registerTask('default', ['copy', 'uglify', 'bytesize', 'watch']);
+
+};
