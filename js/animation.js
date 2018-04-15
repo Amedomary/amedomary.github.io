b0521243061fdb49613097931ac6ffcf10507625
Maxim G <amed@mail.ru>
2018-04-15 22:07:33 +0400

    minificate

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,1 @@
-require(['./config',], function() {
-    require([
-    ]);
-});
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJhbmltYXRpb24uanMiXSwic291cmNlc0NvbnRlbnQiOlsicmVxdWlyZShbJy4vY29uZmlnJyxdLCBmdW5jdGlvbigpIHtcclxuICAgIHJlcXVpcmUoW1xyXG4gICAgXSk7XHJcbn0pOyJdLCJmaWxlIjoiYW5pbWF0aW9uLmpzIn0=
+require(["./config"],function(){require([])});
--- js/main.js
+++ js/main.js
@@ -1,6 +1,1 @@
-require(['./config',], function() {
-    require([
-        'partials/background'
-    ]);
-});
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbInJlcXVpcmUoWycuL2NvbmZpZycsXSwgZnVuY3Rpb24oKSB7XHJcbiAgICByZXF1aXJlKFtcclxuICAgICAgICAncGFydGlhbHMvYmFja2dyb3VuZCdcclxuICAgIF0pO1xyXG59KTsiXSwiZmlsZSI6Im1haW4uanMifQ==
+require(["./config"],function(){require(["partials/background"])});
