5571eb22af936d0f2fe623350913aafa88b317c0
Maksim Grishin <maksim.grishin@simbirsoft.com>
2020-07-28 20:06:57 +0400

    v10.0.4

--- js/partials/background.js
+++ js/partials/background.js
@@ -2,146 +2,152 @@
 // Открытие попАпа и запрет скрола на body
 // ====================
 
-define(["jquery"], function($) {
-  const $backgroundLink = $(".js-background-link"); // Ссылка на картинку
-  const $listItemLink = $(".js-background-link").parent();
-  const $backgroundImage = $(".js-background"); //картинка
-  const $backgroundWrapper = $(".js-background-wrapper"); //обёртка фонов
-  const $backgroundClose = $(".js-background-link-close"); //Кнопка очистки слайдера
-  const $mainNavButton = $(".js-main-nav"); //кнопочка для переходу на страницу
-  const $mainNavButtonText = $(".js-main-nav .text"); // текст кнопки для переходу на страницу
+define(['jquery'], function ($) {
+  const $backgroundLink = $('.js-background-link'); // Ссылка на картинку
+  const $listItemLink = $('.js-background-link').parent();
+  const $backgroundImage = $('.js-background'); //картинка
+  const $backgroundWrapper = $('.js-background-wrapper'); //обёртка фонов
+  const $backgroundClose = $('.js-background-link-close'); //Кнопка очистки слайдера
+  const $mainNavButton = $('.js-main-nav'); //кнопочка для переходу на страницу
+  const $mainNavButtonText = $('.js-main-nav .text'); // текст кнопки для переходу на страницу
   const textBefore = $mainNavButtonText.text(); //текст До
-  const $sfx = document.querySelector(".line-sfx"); // Кружочек спецэфектов
-  const textAfter = "Show more"; //текст После
-  const $mainNavList = $(".js-main-list-nav");
-  const $descriptionBlock = $(".js-description");
-  const $descriptionText = $(".js-description-block p");
-  const $tagBar = $(".b-main-nav__tag-bar");
-  const $visitDirectLink = $(".js-visit-direct-link");
+  const $sfx = document.querySelector('.line-sfx'); // Кружочек спецэфектов
+  const textAfter = 'Show more'; //текст После
+  const $mainNavList = $('.js-main-list-nav');
+  const $descriptionBlock = $('.js-description');
+  const $descriptionText = $('.js-description-block p');
+  const $tagBar = $('.b-main-nav__tag-bar');
+  const $visitDirectLink = $('.js-visit-direct-link');
   let $backgroundImageData; // Дата для фона
   let colorButtonData; // Цвет для кнопки из ссылки
   let dataHrefMainNavigation; //дата для передачи ссылки в другую кнопку
   let xy; // $sfx геометрич значение
 
   const state = {
-    backgroundIsOpen: false
+    backgroundIsOpen: false,
   };
 
   const dataDescription = {
     id_login: {
-      text: "Форма входа на сайт",
-      tags: ["Animation", "Vue.JS", "UI/UX"]
+      text: 'Форма входа на сайт',
+      tags: ['Animation', 'Vue.JS', 'UI/UX'],
     },
     id_timer: {
       text:
-        "Приложение для запуска таймера с возможностью поделиться своим событием",
-      tags: ["SPA", "Vue.JS", "UI/UX", "Alarm", "Animation", "firebase"]
+        'Приложение для запуска таймера с возможностью поделиться своим событием',
+      tags: ['SPA', 'Vue.JS', 'UI/UX', 'Alarm', 'Animation', 'firebase'],
     },
     id_css_vars: {
       text: `
         <p>CSS переменные и цветовая тема для сайта в несколько строк</p>
         <a href="https://habr.com/ru/post/466587/" target="_blank">Читать на Habr</a>
       `,
-      tags: ["CSS", "post", "habr"]
+      tags: ['CSS', 'post', 'habr'],
     },
     id_fractus: {
-      text: "Динамическое создание фракталов на JS",
-      tags: ["mathematic", "geometry", "algorithm", "canvas"]
+      text: 'Динамическое создание фракталов на JS',
+      tags: ['mathematic', 'geometry', 'algorithm', 'canvas'],
     },
     id_game: {
-      text: "2D игра с поддержкой геймпадов",
-      tags: ["2D", "game", "canvas"]
+      text: '2D игра с поддержкой геймпадов',
+      tags: ['2D', 'game', 'canvas'],
     },
     id_xcom: {
-      text: "Стратегическая пошаговая игра в жанре X-com",
-      tags: ["2D", "game", "canvas"]
+      text: `
+      <p>2D Игра</p>
+      <p><a href="//amedomary.github.io/myFirstGame/" target="_blank">Visit Direct Link (2D)</a></p>
+
+      <p>Стратегическая пошаговая игра в жанре X-com</p>
+    `,
+      tags: ['2D', 'game', 'canvas'],
     },
     id_animation: {
-      text: "Возможности стоковых и не только анимаций на CSS и JS",
-      tags: ["CSS", "animation"]
+      text: 'Возможности стоковых и не только анимаций на CSS и JS',
+      tags: ['CSS', 'animation'],
     },
     id_chart: {
-      text: "Создание графиков и их отрисовка",
-      tags: ["mathematic", "geometry", "algorithm", "canvas"]
+      text: 'Создание графиков и их отрисовка',
+      tags: ['mathematic', 'geometry', 'algorithm', 'canvas'],
     },
     id_calculator: {
-      text: "Красивый калькулятор",
-      tags: ["mathematic"]
+      text: 'Красивый калькулятор',
+      tags: ['mathematic'],
     },
     id_form: {
-      text: "Аудиовизуализатор (эквалайзер)",
-      tags: ["Canvas", "Audio", "Animation", "UX"]
+      text: 'Аудиовизуализатор (эквалайзер)',
+      tags: ['Canvas', 'Audio', 'Animation', 'UX'],
     },
     id_blog: {
-      text: "Страница блога",
-      tags: ["markdown", "CSS-vars", "themes"]
+      text: 'Страница блога',
+      tags: ['markdown', 'CSS-vars', 'themes'],
     },
     id_portfolio: {
-      text: "Продолжение списка моих проектов",
-      tags: ["markdown"]
+      text: 'Продолжение списка моих проектов',
+      tags: ['markdown'],
     },
     id_3d: {
-      text: "Расширение для touch bar (macbook pro) под VS Code",
-      tags: ["VS Code", "Extention", "TS"]
+      text: 'Расширение для touch bar (macbook pro) под VS Code',
+      tags: ['VS Code', 'Extention', 'TS'],
     },
     id_prerender: {
       text: `
         <p>Возможность пререндера на js canvas 2D</p>
         <a href="https://habr.com/ru/users/amedomary/posts/" target="_blank">Читать на Habr</a>
       `,
-      tags: ["2D", "Canvas"]
+      tags: ['2D', 'Canvas'],
     },
     id_beercoin: {
-      text: "Подкинь монетку и пей бесплатно! Закрути свою удачу и получи скидку в случайном баре.",
-      tags: ["3D", "CSS", "React", "Start up"]
+      text:
+        'Подкинь монетку и пей бесплатно! Закрути свою удачу и получи скидку в случайном баре.',
+      tags: ['3D', 'CSS', 'React', 'Start up'],
     },
 
     // default
     id_: {
-      text: "",
-      tags: []
-    }
+      text: '',
+      tags: [],
+    },
   };
 
   function updateCanvas() {
     properties.partColor = `rgba(${colorButtonData}, 1)`;
     properties.partLineSubColor = `rgba(${colorButtonData}, `;
-    $("html").css({ "--canvas-color": `rgba(${colorButtonData}, 1)` });
+    $('html').css({ '--canvas-color': `rgba(${colorButtonData}, 1)` });
   }
 
   function enableDescription(text, tags) {
     $descriptionText.html(text);
 
-    $tagBar.html("");
-    tags.forEach(element => {
+    $tagBar.html('');
+    tags.forEach((element) => {
       $tagBar.append(`<span class="b-main-nav__tag">${element}</span>`);
     });
 
-    $descriptionBlock.addClass("active");
-    $mainNavList.addClass("active");
+    $descriptionBlock.addClass('active');
+    $mainNavList.addClass('active');
   }
 
   function disableDescription() {
-    $descriptionBlock.removeClass("active");
-    $mainNavList.removeClass("active");
+    $descriptionBlock.removeClass('active');
+    $mainNavList.removeClass('active');
   }
 
   // Дизейблит кнопку перехода на др страницу
   function disableNavButton() {
-    $mainNavButton.addClass("disable");
+    $mainNavButton.addClass('disable');
     $mainNavButtonText.text(textBefore);
-    $mainNavButton.css("background-color", "transparent");
-    $mainNavButton.css("border-color", "transparent");
+    $mainNavButton.css('background-color', 'transparent');
+    $mainNavButton.css('border-color', 'transparent');
     state.backgroundIsOpen = false;
   }
 
   // Активирует кнопку перехода на др страницу
   function enableNavButton() {
-    $mainNavButton.removeClass("disable");
+    $mainNavButton.removeClass('disable');
     $mainNavButtonText.text(textAfter);
-    $mainNavButton.css("border-color", `rgba(${colorButtonData}, .2)`);
-    $mainNavButton.css("background-color", `rgba(${colorButtonData}, .1)`);
-    $mainNavButton.attr("href", dataHrefMainNavigation);
+    $mainNavButton.css('border-color', `rgba(${colorButtonData}, .2)`);
+    $mainNavButton.css('background-color', `rgba(${colorButtonData}, .1)`);
+    $mainNavButton.attr('href', dataHrefMainNavigation);
     state.backgroundIsOpen = true;
   }
 
@@ -166,7 +172,7 @@ define(["jquery"], function($) {
 
     // alert('dsd ');
 
-    $sfx.classList.add("active");
+    $sfx.classList.add('active');
     // styles
     $sfx.style.transition = `all ${animationTime}s, opacity .2s`;
     $sfx.style.top = `${xy.top - 4}px`;
@@ -176,15 +182,15 @@ define(["jquery"], function($) {
   }
 
   function goOutBtnAnimation() {
-    $sfx.classList.remove("active");
+    $sfx.classList.remove('active');
   }
 
   function closeBackground() {
-    $backgroundWrapper.removeClass("active");
-    $backgroundLink.removeClass("active");
-    $backgroundImage.removeClass("show");
-    $listItemLink.removeClass("active-li");
-    $backgroundClose.removeClass("active");
+    $backgroundWrapper.removeClass('active');
+    $backgroundLink.removeClass('active');
+    $backgroundImage.removeClass('show');
+    $listItemLink.removeClass('active-li');
+    $backgroundClose.removeClass('active');
     goOutBtnAnimation();
     disableNavButton();
     disableDescription();
@@ -192,13 +198,13 @@ define(["jquery"], function($) {
   }
 
   // Клик по клавише
-  $(document).on("keydown", function(event) {
+  $(document).on('keydown', function (event) {
     // Клик по табу
     if (event.keyCode === 9) {
       if (state.backgroundIsOpen) {
         setTimeout(() => {
-          let element = $(":focus");
-          if (element.hasClass("js-background-link")) {
+          let element = $(':focus');
+          if (element.hasClass('js-background-link')) {
             element.click();
           }
         }, 0);
@@ -214,30 +220,28 @@ define(["jquery"], function($) {
   });
 
   // клик по ссылке для фона
-  $backgroundLink.on("click", function(event) {
+  $backgroundLink.on('click', function (event) {
     $backgroundImageData = $(
       ".js-background[data-background='" +
-        $(this).attr("data-background") +
+        $(this).attr('data-background') +
         "']"
     ); // Забираем дату из ссылки клика и закидываем в переменную картинки
-    colorButtonData = $(this).attr("data-color"); // Забираем цвет фона кнопки из даты
-    dataHrefMainNavigation = $(this).attr("data-href"); // забираем адресс ссылки из даты
-    idElement = $(this).attr("data-element-id");
+    colorButtonData = $(this).attr('data-color'); // Забираем цвет фона кнопки из даты
+    dataHrefMainNavigation = $(this).attr('data-href'); // забираем адресс ссылки из даты
+    idElement = $(this).attr('data-element-id');
 
     // Если картинка со ссылкой ещё не октрыты (не совпадает ссылка с открытой фоткой)
-    if (!$backgroundImageData.hasClass("show")) {
+    if (!$backgroundImageData.hasClass('show')) {
       event.preventDefault();
       // делаем кнопку и li активной
-      $backgroundLink.removeClass("active");
-      $listItemLink.removeClass("active-li");
-      $(this).addClass("active");
-      $(this)
-        .parent()
-        .addClass("active-li");
-      $backgroundWrapper.addClass("active"); // Добавляем обёртке атив
-      $backgroundImage.removeClass("show"); // скрываем все фоны
-      $backgroundImageData.addClass("show"); // показываем конкретный фон
-      $backgroundClose.addClass("active"); // показываем кнопку закрыть
+      $backgroundLink.removeClass('active');
+      $listItemLink.removeClass('active-li');
+      $(this).addClass('active');
+      $(this).parent().addClass('active-li');
+      $backgroundWrapper.addClass('active'); // Добавляем обёртке атив
+      $backgroundImage.removeClass('show'); // скрываем все фоны
+      $backgroundImageData.addClass('show'); // показываем конкретный фон
+      $backgroundClose.addClass('active'); // показываем кнопку закрыть
       updateCanvas();
       enableNavButton();
       enableDescription(
@@ -252,7 +256,7 @@ define(["jquery"], function($) {
   });
 
   // Кликаем по закрывашки фона
-  $backgroundClose.on("click", function() {
+  $backgroundClose.on('click', function () {
     closeBackground();
   });
 });
